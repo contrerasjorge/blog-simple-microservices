b16205b60c535ce7a63d9ed35f575f6ea2152905
Jorge Contreras <jorge.contreras789@gmail.com>
2020-04-25 06:45:53 -0400

    Created emitted event handler

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const { randomBytes } = require("crypto");
-const cors = require("cors");
+const express = require('express');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -8,12 +9,12 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
-  const commentId = randomBytes(4).toString("hex");
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
@@ -22,9 +23,24 @@ app.post("/posts/:id/comments", (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+
+  app.post('/events', (req, res) => {
+    console.log('Event Received', req.body.type);
+
+    res.send({});
+  });
 });
 
 app.listen(4001, () => {
-  console.log("Listening on port 4001 🧐");
+  console.log('Listening on port 4001 🧐');
 });
